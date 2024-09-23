71a6accb4a6022e83fd7138492f02c4122270839
hamzakhalem <hamzakhalem97@gmail.com>
2024-09-23 17:10:10 +0100

    new

--- main/product/views.py
+++ main/product/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from .model import Product
 # Create your views here.
+
+@api_view(['GET'])
+def get_all_products(request):
+    products = Product.objects.all()
+    return Response({})
