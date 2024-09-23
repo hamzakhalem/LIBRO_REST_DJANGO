0f1db5c5f26e393d08790c057b89b607e26f7386
hamzakhalem <hamzakhalem97@gmail.com>
2024-09-23 17:12:54 +0100

    get products api

--- main/product/urls.py
+++ main/product/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .view import get_all_products
+from .views import get_all_products
 urlpatterns = [
     path('products/', get_all_products, name='products')
 ]
--- main/product/views.py
+++ main/product/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .model import Product
+from .models import Product
 # Create your views here.
 
 @api_view(['GET'])
 def get_all_products(request):
     products = Product.objects.all()
-    return Response({})
+    return Response({'hello':'hello'})
