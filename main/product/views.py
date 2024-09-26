62445bdda4a50c6bf6abac37b7dadbefbfc0ff15
hamzakhalem <hamzakhalem97@gmail.com>
2024-09-26 14:31:30 +0100

    pagination

--- main/product/views.py
+++ main/product/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Product
 from .serializers import PoductSerializer
@@ -10,8 +11,13 @@ from .filters import ProductFilter
 @api_view(['GET'])
 def get_all_products(request):
     filterset = ProductFilter(request.GET, queryset=Product.objects.all())
-    serializer = PoductSerializer(filterset.qs, many=True)
-    return Response({'product': serializer.data})
+    count = filterset.qs.count()
+    resPage = 1
+    paginator = PageNumberPagination() 
+    paginator.page_size = resPage
+    queryset = paginator.paginate_queryset(filterset.qs, request)
+    serializer = PoductSerializer(queryset, many=True)
+    return Response({'product': serializer.data, 'perpage': resPage, 'total': count })
 
 @api_view(['GET'])
 def get_by_id_product(request, pk):
