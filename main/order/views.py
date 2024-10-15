2bf0f8039e8375c313ffe586f78cb751801b52bf
hamzakhalem <hamzakhalem97@gmail.com>
2024-10-15 16:17:47 +0100

    fix: backup upload

--- main/order/views.py
+++ main/order/views.py
@@ -3,7 +3,7 @@ from django.db.models import Avg
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework import status
 
 from .models import Order, OrderItem
